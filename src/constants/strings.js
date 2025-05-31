2a3af7c68712f313e3c2c513ebf7a7bd23c3723c
Shahidul Deowan <deowanshahidul@gmail.com>
2025-05-31 12:20:32 +0600

    feat: add labels for product details in strings.js

---
+++ src/constants/strings.js
@@ -0,0 +1,9 @@
+export const labels = {
+  metadataTitle: "Product Details",
+  salesRank: "Sales Rank",
+  dateAdded: "Date Added",
+  dimensions: "Dimensions",
+  weight: "Weight",
+  asin: "ASIN",
+  origin: "Origin",
+};
