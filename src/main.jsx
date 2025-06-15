cb5e87f9487de38b98fbc56e7e19fe5120ecb1e0
Shahidul Deowan <deowanshahidul@gmail.com>
2025-06-16 03:04:10 +0600

    feat: replace createRoot with dom.injectReactComponent for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.jsx";
+import dom from "./lib/dom.js";
 
-createRoot(document.getElementById('root')).render(
+dom.injectReactComponent(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+  document.getElementById("root"),
+  {
+    isRoot: true,
+  }
+);
