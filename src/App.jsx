4d8689648f8df6fa52c5378c6ee6029da5f38e32
Shahidul Deowan <deowanshahidul@gmail.com>
2025-06-16 18:54:42 +0600

    feat: simplify Popup component by removing commented code and redundant elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 function Popup() {
   return (
     <div className="relative p-4 w-[350px] bg-background">
-
       <div className="">
-        {/* <div className="w-full  h-20 overflow-hidden ">
-            <img
-              className="mx-auto h-20 w-auto"
-              src={'icons/icon128.png'}
-              width={150}
-              height={150}
-            />
-          </div> */}
         <div className="text-center">
-          <h1 className=" font-bold text-3xl text-white">
-            🔮 Amazon Scanner
+          <h1 className=" font-bold text-3xl">
+            🔮 Amazon Scanner{" "}
+            <span className="text-sm text-muted-foreground">
+              Ultra-premium Amazon insights.
+            </span>
           </h1>
-          <p className="text-sm text-muted-foreground">
-            Ultra-premium Amazon insights.
-          </p>
         </div>
       </div>
-
     </div>
   );
 }
