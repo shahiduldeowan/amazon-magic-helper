fd0e600d2dbffc3e7cc921309f59427ec8a6a0b0
Shahidul Deowan <deowanshahidul@gmail.com>
2025-06-16 18:55:31 +0600

    fix: add missing sender and sendResponse parameters to onMessage listener for consistency

--- src/background/background.js
+++ src/background/background.js
@@ -1,5 +1,6 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'openPopup') {
-        chrome.action.openPopup() // Opens the popup programmatically
-    }
-})
+/* global chrome */
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "openPopup") {
+    chrome.action.openPopup(); // Opens the popup programmatically
+  }
+});
