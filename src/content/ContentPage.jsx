0a48556a720fdcf33edb363c0764efbb467089ad
Shahidul Deowan <deowanshahidul@gmail.com>
2025-06-27 12:54:17 +0600

    feat: refactor ContentPage to use DomUtils for product data retrieval and inject analysis panel

--- src/content/ContentPage.jsx
+++ src/content/ContentPage.jsx
@@ -1,38 +1,69 @@
-import { useEffect } from "react";
+import { StrictMode, useEffect } from "react";
 import FloatingActionButton from "../components/FAB/FloatingActionButton";
-import dom from "../lib/dom";
-import { QUERY_SELECTORS } from "../constants/selectors";
+import { DomUtils, logInfo } from "../utils";
+import { CREATED_ENTITY, QUERY_SELECTORS } from "../constants/selectors";
+import { getAmazonAllProductData } from "../lib/amazon/extractAmazonProductDataUtils";
+import ProductMetadataCard from "../components/product/ProductMetadataCard";
+import { NicheAnalysisPanel } from "../components/product/NicheAnalysisPanel";
+import { sampleAnalysis } from "../constants/strings";
 
 const ContentPage = () => {
+  // Function to inject the React component into the page
+  function injectAnalysisPanel() {
+    DomUtils.injectReactComponent(
+      <StrictMode>
+        <NicheAnalysisPanel analysis={sampleAnalysis} />
+      </StrictMode>,
+      document.querySelector("#nav-main"),
+      {
+        id: CREATED_ENTITY.PRODUCT_ANALYTICS_CARD_ID,
+        className: "amz-niche-analysis-panel",
+        afterend: true,
+      }
+    );
+  }
+
   useEffect(() => {
-    dom
-      .waitForElement(QUERY_SELECTORS.SEARCH_PRODUCT_CONTAINERS, 5000)
-      .then(() => {
-        const productContainers = dom.findAmazonProductContainers();
-
-        console.log("Product containers found:", productContainers.length);
-
-        if (productContainers.length > 0) {
-          const products = productContainers.map((element) =>
-            dom.getProductData(element)
-          );
-          console.log("Product data:", products);
-          // productContainers.forEach((container, index) => {
-          //   if (index < 6) {
-          //     const product = dom.getProductData(container);
-          //     console.log(
-          //       `-->[${index}]Product data: ${JSON.stringify(product)}`
-          //     );
-          //   }
-          // });
+    DomUtils.waitForElement(QUERY_SELECTORS.SEARCH_PRODUCT_CONTAINERS, 5000)
+      .then(async () => {
+        injectAnalysisPanel();
+        const products = await getAmazonAllProductData();
+        logInfo("Products found: ", products.length);
+        logInfo("Products: ", products);
+        if (products.length > 0) {
+          products.forEach((product) => {
+            const el = DomUtils.qs(
+              QUERY_SELECTORS.INNER_SECTION.S_1,
+              product.element
+            );
+            if (el) {
+              const metadata = {
+                salesRank: product.bestSellersRank?.bsr || "N/A",
+                dateAdded: product.dateFirstAvailable || "N/A",
+                dimensions: product.dimensions || "N/A",
+                weight: product.weight || "N/A",
+                asin: product.asin || "N/A",
+                countryOfOrigin: product.countryOfOrigin || "N/A",
+              };
+              DomUtils.injectReactComponent(
+                <StrictMode>
+                  <ProductMetadataCard metadata={metadata} />
+                </StrictMode>,
+                el,
+                {
+                  id: CREATED_ENTITY.PRODUCT_METADATA_CARD_ID,
+                  className: "amz-product-metadata-card",
+                  prepend: true,
+                }
+              );
+            }
+          });
         }
       })
       .catch((error) => console.error("Error waiting for products", error));
   }, []);
 
-  const injectProductSpecs = () => {
-    console.log("Injecting product specs............");
-  };
+  const injectProductSpecs = () => {};
 
   return (
     <div className="fixed bottom-8 right-8 z-50">
