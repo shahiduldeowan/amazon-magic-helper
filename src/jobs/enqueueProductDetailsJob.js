603faf4266df7cb1b2b6c8ff2176896ba8aadeb6
Shahidul Deowan <deowanshahidul@gmail.com>
2025-07-06 10:32:47 +0600

    feat: enhance enqueueProductDetailsJob to support callback and improve error handling

--- src/jobs/enqueueProductDetailsJob.js
+++ src/jobs/enqueueProductDetailsJob.js
@@ -1,22 +1,34 @@
 import { extractProductDetails } from "../lib/amazon/extractAmazonProductDataUtils";
 import { delay, fetchHTML, logInfo } from "../utils";
 
-export async function enqueueProductDetailsJob(products, batchSize = 10) {
+export async function enqueueProductDetailsJob(
+  products,
+  callback = () => {},
+  batchSize = 10
+) {
   const queue = [...products];
   while (queue.length > 0) {
     const batch = queue.splice(0, batchSize);
     await Promise.all(
-      batch.map(async (product) => {
-        const doc = await fetchHTML(product.productUrl || "");
-        const detailData = extractProductDetails(doc);
-        if (detailData) {
-          Object.assign(product, detailData);
-          logInfo(`[Combined Data] ${product.asin}`);
+      batch.map(async (product, index) => {
+        try {
+          const doc = await fetchHTML(product.productUrl || "");
+          const detailData = extractProductDetails(doc);
+          if (detailData) {
+            Object.assign(product, detailData);
+            callback(product);
+            logInfo(`[Combined Data] ${product.asin}`);
+          }
+          await delay(200 + Math.random() * 100);
+        } catch (error) {
+          logInfo(
+            `[enqueueProductDetailsJob (${index}) ] Error processing product ${product.asin}:`,
+            error
+          );
         }
-        await delay(200 + Math.random() * 100);
       })
     );
-    await delay(1000 + Math.random() * 500); // batch delay
+    await delay(600 + Math.random() * 500); // batch delay
   }
   return products;
 }
